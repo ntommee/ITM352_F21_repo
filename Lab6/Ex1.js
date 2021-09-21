9e9b60dc190d9741c55a86515604974432e2a197
ntommee <ntommee@hawaii.edu>
2021-09-21 11:49:18 -1000

    Lab6

---
+++ Lab6/Ex1.js
@@ -0,0 +1,8 @@
+var age =  20;
+var fav_num = 3;
+var day_of_birth = 3;
+var month_of_birth =  6;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+
+console.log((age + fav_num) / day_of_birth * month_of_birth);
