9eeaa6b68726ea3aa44564fb4e36bb8143ae01fa
ntommee <ntommee@hawaii.edu>
2021-10-28 20:19:45 -1000

    update

---
+++ WODs/Invoice4/products_data.js
@@ -0,0 +1,26 @@
+console.log("Product variables loaded...");
+
+var products = [
+
+    { name: "HTC",
+    price: 40.00,
+     image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg"},
+
+     { name: "Apple",
+     price: 75.00,
+     image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg"},
+
+     { name: "Nokia",
+     price: 35.00,
+     image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg"},
+
+     { name: "Samsung",
+     price: 45.00,
+     image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg"},
+
+     { name: "Blackberry",
+     price: 10.00,
+     image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg"}   
+
+
+];
