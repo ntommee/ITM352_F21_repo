e4771ff42c0d43855e0a3e06af960c8d139a1883
ntommee <ntommee@hawaii.edu>
2021-11-14 17:10:46 -1000

    update

--- Tommee_Nicole_Assignment1/server.js
+++ Tommee_Nicole_Assignment1/server.js
@@ -1,56 +1,63 @@
-var products_array = require('./product_data.js');
-
 var express = require('express');
 var app = express();
 
-// Routing 
-app.get('/products_display.html', function (req, res, next) {
-
-    function isNonNegInt(q, return_errors = false) {
-        errors = []; // assume no errors at first
-        if (q == '') q = 0; // handle blank inputs as if they are 0
-        if (Number(q) != q) errors.push('<font color="red">Not a number!</font>'); // Check if string is a number value
-        if (q < 0) errors.push('<font color="red">Negative value!</font>'); // Check if it is non-negative
-        if (parseInt(q) != q) errors.push('<font color="red">Not an integer!</font>'); // Check that it is an integer
-        return return_errors ? errors : (errors.length == 0);
-    }
-
-    data = require('./static/product_data.js');
-    products = data.products;
-
-    if (typeof req.query['purchase_submit'] != 'undefined') {
-        /*
-        for (i = 0; i < products.length; i++) {
-            if (params.has(`quantity${i}`)) {
-                a_qty = params.get(`quantity${i}`);
-                // make textboxes sticky in case of invalid data
-                product_selection_form[`quantity${i}`].value = a_qty;
-                total_qty += a_qty;
-                if (!isNonNegInt(a_qty)) {
-                    has_errors = true; // oops, invalid quantity
-                    checkQuantityTextbox(product_selection_form[`quantity${i}`]); // show where the error is
-                }
-            }
-        }
-        */
-        console.log(Date.now() + ': Purchase made from ip ' + req.ip + ' data: ' + JSON.stringify(req.query));
-    }
-    next();
-});
+var products = require('./products.json');
+products.forEach((prod, i) => { prod.total_sold = 0 });
 
 // monitor all requests
 app.all('*', function (request, response, next) {
-   console.log(request.method + ' to ' + request.path);
-   next();
+    console.log(request.method + 'to path' + request.path + 'query string' + JSON.stringify(request.query));
+    next();
 });
 
-/*process purchase request (validate quantities, check quantity available)
-    insert code here
+// routing
+app.get("/product_data.js", function (request, response, next) {
+    response.type('.js');
+    var products_str = `var products = ${JSON.stringify(products)};`;
+    response.send(products_str);
+});
+
+function isNonNegInt(q, returnErrors = false) {
+    errors = []; // assume no errors at first
+    if (q == '') q = 0;
+    if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+    else {
+        if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
+        if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+    }
+    return returnErrors ? errors : (errors.length == 0);
+}
+
+// get the body
+app.use(express.urlencoded({ extended: true }));
+
+/*app.post("/process_form", function (request, response) {
+    response.send(request.body);
+});
 */
 
+// process purchase request (validate quantities, check quantity available) 
+app.post('/process_form', function (request, response, next) {
+    for (i = 0; i < products.length; i++) {
+        let name = products[i]['name'];
+        let name_price = products[i]['price'];
+        var q = request.body[`quantity${i}`];
+        if (typeof q != 'undefined') {
+            if (isNonNegInt(q)) {
+                products[0].total_sold += Number(q);
+                response.send(`Thank you for purchasing ${q} ${name}. Your total is \$${q * name_price}!`);
+            } else {
+                response.send(`Error: ${q} is not a quantity. Hit the back button to fix..`)
+            }
+        } else {
+            response.send(`Hey! You need to pick some stuff!`)
+        };
+    }
+});
+
 // route all other GET requests to files in public 
-app.use(express.static('./public'));
+app.use(express.static('./public')); // essentially replaces http-server
 
 // start server
-app.listen(8080, () => console.log(`listening on port 8080`));
+app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
 
