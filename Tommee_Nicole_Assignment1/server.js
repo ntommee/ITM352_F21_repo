afe51aa6182eb9a19cdb288a429494aa8ee5b781
ntommee <ntommee@hawaii.edu>
2021-11-15 12:31:56 -1000

    update

--- Tommee_Nicole_Assignment1/server.js
+++ Tommee_Nicole_Assignment1/server.js
@@ -1,10 +1,13 @@
 /* 
 * Nicole Tommee
 * Displays product data, validates the input, and presents an invoice 
+* Used code from Lab13 Ex4 & Assignment1_MVC_server for guidance
 */
 
 var express = require('express');
 var app = express();
+var myParser = require("body-parser");
+var fs = require('fs');
 
 // get the body
 app.use(express.urlencoded({ extended: true }));
@@ -31,25 +34,61 @@ app.get("/product_data.js", function (request, response, next) {
     response.send(request.body);
 });
 */
-
-// process purchase request (validate quantities, check quantity available) 
-app.post('/process_form', function (request, response, next) {
-    for (i = 0; i < products.length; i++) {
-        let name = products[i]['name'];
-        let name_price = products[i]['price'];
-        var q = request.body[`quantity${i}`];
-        if (typeof q != 'undefined') {
-            if (isNonNegInt(q)) {
-                products[0].total_sold += Number(q);
-                response.send(`Thank you for purchasing ${q} ${name}. Your total is \$${q * name_price}!`);
-            } else {
-                response.send(`Error: ${q} is not a quantity. Hit the back button to fix..`)
-            }
-        } else {
-            response.send(`Hey! You need to pick some stuff!`)
-        };
+app.post("/process_form", function (request, response, next) {
+    let POST = request.body;
+    // if you try to direct to the invoice page without having clicked the purchase button, show the error message
+    if (typeof POST['purchase_submit'] == 'undefined') {
+        response.send("Please purchase some items first!");
+        console.log('No purchase form data');
         next();
     }
+
+    console.log(Date.now() + ': Purchase made from ip ' + request.ip + ' data: ' + JSON.stringify(POST));
+
+    var contents = fs.readFileSync('./invoice.template', 'utf8');
+    response.send(eval('`' + contents + '`')); // render template string
+
+    function display_invoice_table_rows() {
+        subtotal = 0;
+        str = '';
+        for (i = 0; i < products.length; i++) {
+            a_qty = 0;
+            if (typeof POST[`quantity${i}`] != 'undefined') {
+                a_qty = POST[`quantity${i}`];
+            }
+            if (a_qty > 0) {
+                // product row
+                extended_price = a_qty * products[i].price
+                subtotal += extended_price;
+                str += (`
+          <tr>
+            <td width="43%">${products[i].name}</td>
+            <td align="center" width="11%">${a_qty}</td>
+            <td width="13%">\$${products[i].price}</td>
+            <td width="54%">\$${extended_price}</td>
+          </tr>
+          `);
+            }
+        }
+        // Compute tax
+        tax_rate = 0.04;
+        tax = tax_rate * subtotal;
+
+        // Compute shipping
+        if (subtotal <= 45) {
+            shipping = 10;
+          } else if (subtotal <= 100) {
+            shipping = 10;
+          } else {
+            shipping = 0.07 * subtotal; // 7% of subtotal
+          }
+
+        // Compute grand total
+        total = subtotal + tax + shipping;
+
+        return str;
+    }
+
 });
 
 // route all other GET requests to files in public 
